6ebd02b928becca6d07c5809cfdcd31d4b905010
SY Young <r38411@yahoo.com>
2016-03-07 19:18:20 +0800

    finish plot2

--- assg1.R
+++ assg1.R
@@ -12,7 +12,7 @@ assg1 <- function() {
     d2 <- paste(data1[,"Date"], data1[,"Time"])
     d2 <- strptime(d2,"%d/%m/%Y %H:%M:%S")
     data1 <- cbind(d2,data1)
-    colnames(data1)[1] <- "Date-Time"
+    colnames(data1)[1] <- "Date_Time"
     
     data1
 }
@@ -22,5 +22,13 @@ plot1 <- function() {
     png(filename="plog1.png", width=480, height=480)
     hist(my_data$Global_active_power, xlab="Global Active Power(kilowatts)", 
          col="red", main="Global Active Power")
+    dev.off()
+}
+
+plot2 <- function() {
+    my_data <- assg1()
+    png(filename="plot2.png",width=480,height=480)
+    with(my_data, plot(x=Date_Time, y=Global_active_power,type="l",
+                       ylab="Global Active Power(kilowatts)")) 
     dev.off()
 }
