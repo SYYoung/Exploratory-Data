122ef9922b38bfb7f02767b2435d65df715e4788
SY Young <r38411@yahoo.com>
2016-03-08 13:04:20 +0800

    finish all plots

--- assg1.R
+++ assg1.R
@@ -47,3 +47,29 @@ plot3 <- function() {
     
     dev.off()
 }
+
+plot4 <- function() {
+    my_data <- read_assg1_data()
+    #png(filename="plot4.png", width=480, height=480)
+    par(mfrow=c(2,2),mar=c(5,4,2,1))
+    
+    #plot 1st: same as plot#2 w/o the title and xlab
+    with(my_data, plot(x=Date_Time, y=Global_active_power,type="l",
+                       xlab=" ", ylab="Global Active Power")) 
+    # plot 2nd
+    with(my_data, plot(x=Date_Time, y=Voltage,type="l",
+                       xlab="datetime", ylab="Voltage"))
+    # plot 3rd: same as plot#3 w/o xlab
+    plot(range(my_data$Date_Time),range(my_data$Sub_metering_1),
+         xlab=" ", ylab="Energy sub metering", type="n")
+    points(my_data$Date_Time,my_data$Sub_metering_1, type="l")
+    points(my_data$Date_Time,my_data$Sub_metering_2, type="l", col="red")
+    points(my_data$Date_Time,my_data$Sub_metering_3, type="l",col="blue")
+    legend("topright", col=c("grey","red", "blue"), pch="----",
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    
+    # plot 4th
+    with(my_data, plot(x=Date_Time, y=Global_reactive_power,type="l",
+                       xlab="datetime", ylab="Global Reactive Power"))
+     
+}
