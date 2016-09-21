e90bb261177a80496db18217ed99bad056c60836
SY Young <r38411@yahoo.com>
2016-09-21 11:15:48 +0800

    finish ch.4

--- from_book_exp_data.R
+++ from_book_exp_data.R
@@ -16,5 +16,32 @@ from_book_exp_data_ch4 <- function() {
     summary(chic.f$pm25tmean2)
     chic.f <- filter(chicago, pm25tmean2>30 & tmpd>80)
     
-    # arrange
+    # arrange: reorder rows of data frame
+    chi <- arrange(chicago, date)
+    chi <- arrange(chicago, desc(date))
+    
+    # rename
+    chicago <- rename(chicago, dewpoint=dptp, pm25=pm25tmean2)
+    
+    # mutate
+    chicago <- mutate(chicago, pm25detrend=pm25-mean(pm25,na.rm=TRUE))
+    
+    # group_by
+    chicago <- mutate(chicago, year=as.POXIXlt(date)$year+1900)
+    years <- group_by(chicago, year)
+    summarise(years, pm25=mean(pm25,na.rm=TRUE),
+              o3=max(o3tmean2, na.rm=TRUE),
+              no2=median(no2mean2,na.rm=TRUE))
+    qq <- quantile(chicago$pm25, seq(0,1,0.2), na.rm=TRUE)
+    chicago <- mutate(chicago, pm25.quint=cut(pm25,qq))
+    quint <- group_by(chicago, pm25.quint)
+    summarize(quint, o3=mean(o3tmean2,na.rm=TRUE),
+              no2=mean(no2tmean2,na.rm=TRUE))
+    
+    # %>%
+    mutate(chicago, pm25.quint=cut(pm25,qq)) %>%
+        group_by(pm25.quint) %>%
+        summarize(o3=mean(o3tmean2,na.rm=TRUE),
+                  no2=mean(no2tmean2,na.rm=TRUE))
+    
 }
